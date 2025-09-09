b55d1f5be2803feeb7b99003d82ae09e87eaf4e5
shammikabir <shamimashammi861@gmail.com>
2025-09-09 15:00:39 +0600

    readme file update

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
+// nav-bar toggle
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
 // category functionaluty
 const loadcategory = () => {
   fetch("https://openapi.programming-hero.com/api/categories")
